da6778b6afbead620584d47cbc465569372966fa
Sander Datema <mail@sanderdatema.nl>
2023-02-08 18:29:33 +0100

    Add image plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import image from "@astrojs/image";
 
 export default defineConfig({
   site: "https://sanderdatema.github.io",
-  integrations: [sitemap()],
+  integrations: [sitemap(), image()]
 });
