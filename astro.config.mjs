c7a2fc387495d8ca71cc0e9c21df9f844c9bc2d2
Sander Datema <mail@sanderdatema.nl>
2023-02-08 18:30:14 +0100

    Add mdx plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   site: "https://sanderdatema.github.io",
-  integrations: [sitemap(), image()]
+  integrations: [sitemap(), image(), mdx()]
 });
