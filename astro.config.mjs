3b01fe0854925b5d8a7fdc319e0901a739b8bc1a
Sander Datema <mail@sanderdatema.nl>
2023-02-08 18:34:21 +0100

    Add flow plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+
+// Not imported
+// https://github.com/astro-community/flow
+
+// https://docs.astro.build/en/guides/integrations-guide/image/
 import image from "@astrojs/image";
+
+// https://docs.astro.build/en/guides/integrations-guide/mdx/
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
