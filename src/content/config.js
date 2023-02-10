fe11cede5cfb649c450daaea979987075881bf9d
Sander Datema <mail@sanderdatema.nl>
2023-02-10 16:41:15 +0100

    2023-02-10 16:41:12

---
+++ src/content/config.js
@@ -0,0 +1,16 @@
+import { defineCollection } from "astro:content";
+import { z, defineCollection } from "astro:content";
+
+const postCollection = defineCollection({
+  schema: z.object({
+    isDraft: z.boolean(),
+    title: z.string(),
+    tags: z.array(z.string()),
+    image: z.string().optional(),
+    author: z.string().default('Anonymous'),
+  }),
+});
+
+export const collections = {
+  posts: postCollection,
+};
