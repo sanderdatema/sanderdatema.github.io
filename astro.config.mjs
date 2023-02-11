14ce1e200788da4302392d3ab74005c87bb38c13
Sander Datema <mail@sanderdatema.nl>
2023-02-11 23:37:55 +0100

    2023-02-11 23:37:53

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,19 @@
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config"
+import sitemap from "@astrojs/sitemap"
 
 // Not imported
 // https://github.com/astro-community/flow
 
 // https://docs.astro.build/en/guides/integrations-guide/image/
-import image from "@astrojs/image";
+import image from "@astrojs/image"
 
 // https://docs.astro.build/en/guides/integrations-guide/mdx/
-import mdx from "@astrojs/mdx";
+import mdx from "@astrojs/mdx"
 
 // https://github.com/astro-community/astro-compress#readme
-import compress from "astro-compress";
+import compress from "astro-compress"
 
 export default defineConfig({
-  site: "https://sanderdatema.github.io",
-  integrations: [sitemap(), image(), mdx(), compress()]
-});
+    site: "https://sanderdatema.github.io",
+    integrations: [sitemap(), image(), mdx(), compress()],
+})
